40c7382f5a0cb179574798010d31d994ef6dc27e
Richard Moss <richard@englishtutorslive.com>
2021-04-06 17:35:38 +0100

    Remove unused deeps from CRA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { CounterProvider } from './example/CounterContext';
