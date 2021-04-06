74008343ea9359013390a6da86ef50d618d26f2b
Richard Moss <richard@englishtutorslive.com>
2021-04-06 16:50:29 +0100

    tidy and add context provider to example folder

---
+++ src/example/CounterContext.jsx
@@ -0,0 +1,29 @@
+import { useState, useContext, createContext, useEffect } from 'react';
+
+const CounterContext = createContext(undefined);
+
+const CounterProvider = ({ children }) => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <CounterContext.Provider value={{ count, setCount }}>
+      {children}
+    </CounterContext.Provider>
+  );
+};
+
+const useCounter = () => {
+  const context = useContext(CounterContext);
+
+  if (context === undefined) {
+    throw new Error('useCounterState must be used within a CounterProvider');
+  }
+  return context;
+};
+
+const UseCounter = ({ children }) => {
+  const { count, setCount } = useCounter();
+  return children({ count, setCount });
+};
+
+export { CounterProvider, useCounter, UseCounter };
