3976d9fcf6ae39aacaed484e4f503fe322d2411d
Richard Moss <richard@englishtutorslive.com>
2021-04-07 11:47:15 +0100

    Update instructions for Context exercise

--- src/exerciseContext/ThemedApp.jsx
+++ src/exerciseContext/ThemedApp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Text, Code } from '@chakra-ui/react';
 import Button from './Button';
 import Hero from './Hero';
 // import { useTheme } from './ThemeProvider';
@@ -9,9 +10,26 @@ const ThemedApp = () => {
    pass it to the onClick of our Button
   */
   return (
-    <Hero>
-      <Button>Theme Switcher</Button>
-    </Hero>
+    <>
+      <Text>
+        1. Finish implementing the ThemeProvider context in{' '}
+        <Code>src/exerciseContext/ThemeProvider.jsx</Code> Hint: There are lots
+        of code comments in this file, read them :-)
+      </Text>
+      <Text>
+        2. In <Code>index.js</Code> implement your provider by wrapping the app.
+      </Text>
+      <Text>
+        3. In <Code>src/exerciseContext/ThemedApp.jsx</Code> use your useTheme
+        hook to get the function that updates the theme state and pass it to the
+        onClick handler of the Button.
+      </Text>
+
+      {/* You'll need to add your "toggleTheme" function to the onClick of this button*/}
+      <Hero>
+        <Button>Theme Switcher</Button>
+      </Hero>
+    </>
   );
 };
 
